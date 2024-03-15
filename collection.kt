1ff9d8ab9ee62f58fb54aeac9ef72824c069c7cd
SilvaniFilho <163529120+SilvaniFilho@users.noreply.github.com>
2024-03-15 08:12:01 -0300

    Create collection.kt

---
+++ collection.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5)
+    println("Numbers: $numbers")
+    
+    val fruits = mutableListOf("Apple", "Banana", "Orange")
+    fruits.add("Mango")
+    println("Fruits: $fruits")
